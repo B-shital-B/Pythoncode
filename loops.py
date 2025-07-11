bab2e0d27551c1786a97cd735b60d1922e08f987
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:33:07 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,30 @@
+#loops
+#while loop
+# count=1
+# while count<=10:
+#     print("hello",count)
+#     count+=1
+# print no from 1 to5
+# i = 5
+# while i >= 6:
+#     print(i)
+#     i -= 1  # <- This line is *outside* the loop
+# print('loop end')
+# print no 1 t0 100
+#[1,4,9,16,25,36,49,64,81,100]
+# i = [1,4,9,16,25,36,49,64,81,100]
+# index=0
+# while index<len(i):
+#     print(i[index])
+#     index+=1
+# i = (1,4,9,16,25,36,49,64,81,100,36)
+# x=36
+# index=0
+
+# while index<len(i):
+#     if(i[index]==x):
+#         print("found:",index)
+#     else:
+#         print("finding")
+#     index+=1
+
