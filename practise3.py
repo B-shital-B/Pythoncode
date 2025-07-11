935ddc64afbe6d28125f5fbddd9739e17470db29
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:31:09 +0530

    Add files via upload

---
+++ practise3.py
@@ -0,0 +1,29 @@
+# movies=['K3','ydhd','danasoure','jurasic parlk']
+# movies.append('XXX')
+# movies.append('my name is khan')
+# movies.append('i am boss')
+# print(movies)
+#palindrom or not
+# product=["madamp"]
+# product1=[121,123,345,444]
+# p1_product=product.copy()
+# p1_product.reverse()
+
+# if(p1_product==product):
+#     print("palindrom")
+# else:
+#     print("not palindrom")
+# word = "madaml"
+# if word == word[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+# tuple proble
+tup=["C","D","A","A","B","B","A"]
+tupl=tup.count("A")
+print("cont of A:",tupl)
+print(tup)
+tup.sort()
+print(tup)
+tup.reverse()
+print(tup)
