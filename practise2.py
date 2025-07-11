1c569b22e28c662c0429b4a4c6668e53bd2353c4
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:30:52 +0530

    Add files via upload

---
+++ practise2.py
@@ -0,0 +1,52 @@
+#wap to 2 input users first name & print its length
+##"""Name=input("Enter your name:")
+##print(Name)
+##print(len(Name))
+##print(type(Name))
+
+# wap to find occurance of $ in a string
+# str1="$ is My $$ moey"
+# print(str1.count("$"))
+#age=int(input("enter your age:"))
+#if (age >= 20):
+   # print("Applicable for vote")
+#elif(age==18):
+   #  print("ypu are great for this")
+#else:
+  #  print ("Not for sure")
+
+# marks if else code
+# marks=int(input("Enter your marks:"))
+# if(marks>=90):
+#    print("grade A")
+# elif(90>marks>=80):
+#     print("grade B")
+# elif(80>marks>=70):
+#      print("grade C")
+# else:
+#   print("grade D")
+# num=int(input("Enter your No:"))
+# if(num%2==0):
+#      print("Num is even")
+# else:
+#      print("Num is odd")
+#Multiplyed by 7
+# no=int(input("Enter your No:"))
+# if(no%7==0):
+#      print("Multipled by 7")
+# else:
+#   print("Not")
+#gretest no
+#a=10,b=20,c=12
+# a=int(input("Enter your no:"))
+# b=int(input("Enter your no:"))
+# c=int(input("Enter your no:"))
+# d=int(input("Enter your no:"))
+# if(a>b and a>c):
+#     print("A is greater")
+# elif(b>c and b>d):
+#     print("B grater")
+# elif(c>d and c>a):
+#     print("C gretest")
+# else:
+#     print ("D is grater")
