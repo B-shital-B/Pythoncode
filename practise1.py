6ecf87eadd43968c48a78c7b560f60e205ee3f58
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:30:13 +0530

    Add files via upload

---
+++ practise1.py
@@ -0,0 +1,31 @@
+Python 3.12.4 (v3.12.4:8e8a4baf65, Jun  6 2024, 17:33:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("Enter 1st no:"))
+Enter 1st no:50
+>>> b=int(input("Enter 2nd no:"))
+Enter 2nd no:100
+>>> sum=a+b
+>>> print(sum)
+150
+>>> side=int(input("Enter side of square:"))
+Enter side of square:4
+>>> area=side*side
+>>> print(area)
+16
+>>> num1=int(input("Enter first no:"))
+Enter first no:10
+>>> num1=float(input("Enter first no:"))
+Enter first no:3.14
+>>> num2=float(input("Enter second no:"))
+Enter second no:7.11
+>>> avrage=num1+num2/2
+>>> print(avrage)
+6.695
+>>> a=int(input("Enter a:"))
+Enter a:10
+>>> b=int(input("Enter b:"))
+Enter b:20
+>>> print(a>b)
+False
+>>> print(a<b)
+True
