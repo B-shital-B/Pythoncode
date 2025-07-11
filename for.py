8233e6f13eaf41051bbef0395a3f3d216fea46f9
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:33:26 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,47 @@
+# list=[2,3,4,5,6]
+# for val in list:
+#     print(val)
+# vaggies=['tomato','chilles','onion','lockie','ladyfinger']
+# for val in vaggies:
+#     print(val)
+# str='my name is shital'
+# for char in str:
+#     print(char)
+# name = "pass with firstclass distinction"
+# for char in name:
+#     if char == 's':
+#         print('s found')
+#         break
+# else:
+#     print('end')
+
+# print(char)
+i = (1, 4, 9, 16, 25, 36, 49, 64, 81,49, 100)
+x = 49
+index = 0
+
+for ele in i:
+   if ele == x:
+       print(i)
+       break
+index += 1
+#range function
+# seq=range(100,0,-1)
+# for  i in seq:
+#     print(i)
+# n=int(input('enter no:'))
+# for i in range(1,11):
+#     print(n*i)
+# n=5
+# sum=0
+# for i in range(1,n+1):
+#     sum +=i
+#     print('total sum:',sum)
+
+# n=4
+# fact=1
+# i=1
+# while i<=n:
+#     fact *= i
+#     i +=1
+#     print('factorial=',fact)
