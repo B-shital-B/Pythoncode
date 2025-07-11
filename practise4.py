4272199b59acb4dd45c1e5741dbe38d734a464e2
B-shital-B <146361928+B-shital-B@users.noreply.github.com>
2025-07-11 22:31:32 +0530

    Add files via upload

---
+++ practise4.py
@@ -0,0 +1,20 @@
+# dict = {
+#     "name":"shital",
+#     "learning":"coding",
+#     "age":34,
+#     "is_adult":True,
+#     "subject":["python","c","cpp"]
+# }
+# print(type(dict))
+student={
+    "name":"shital bhadre",
+    "subject":{
+        "phy":98,
+        "chem":97,
+        "math":95
+    }
+}
+#nested dictionary
+float(9)
+print(student)
+print
